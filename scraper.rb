91211063f403fb4313625556ada6844be3563ec4
Matthew Landauer <mlandauer@gmail.com>
2019-05-28 13:04:42 +1000

    Update to latest atdisplanningalertsfeed gem

--- scraper.rb
+++ scraper.rb
@@ -3,4 +3,4 @@ Bundler.require
 
 url = "https://mycouncil.warrumbungle.nsw.gov.au:6443/Horizon/@@horizondap@@/atdis/1.0/"
 
-ATDISPlanningAlertsFeed.save(url)
+ATDISPlanningAlertsFeed.save(url, "Sydney")
