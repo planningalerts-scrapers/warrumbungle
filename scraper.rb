020c9dabf0445fc448e8c5f6050784142eb4d414
Matthew Landauer <mlandauer@gmail.com>
2021-05-27 10:05:44 +1000

    Comment out scraper to reduce noise
    https://github.com/planningalerts-scrapers/issues/issues/90

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/env ruby
-Bundler.require
+# Bundler.require
+#
+# url = "https://mycouncil.warrumbungle.nsw.gov.au:6443/Horizon/@@horizondap@@/atdis/1.0/"
+#
+# ATDISPlanningAlertsFeed.save(url, "Sydney")
 
-url = "https://mycouncil.warrumbungle.nsw.gov.au:6443/Horizon/@@horizondap@@/atdis/1.0/"
-
-ATDISPlanningAlertsFeed.save(url, "Sydney")
+puts "Scraper is broken. So it's commented out to reduce noise"
+puts "See https://github.com/planningalerts-scrapers/issues/issues/90"
